e72d699e9173dde20faeb80a49b022b78ab67686
Dylan <dylanlee0613@gmail.com>
2024-08-17 16:30:10 -0700

    initial commit

---
+++ firebaseAuth.js
@@ -0,0 +1,107 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVIQW2H64al26iDOgyj_GhVReqwUQ1DMg",
+  authDomain: "financialapp-47aab.firebaseapp.com",
+  databaseURL: "https://financialapp-47aab-default-rtdb.firebaseio.com",
+  projectId: "financialapp-47aab",
+  storageBucket: "financialapp-47aab.appspot.com",
+  messagingSenderId: "1041157429079",
+  appId: "1:1041157429079:web:3b8868693694ca613c1012",
+  measurementId: "G-L86K9E75Q7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore();
+
+//showing message function for logging in, logging out, errors, etc
+function showMessage(message, divId){
+    var messageDiv = document.getElementById(divId);
+    messageDiv.style.display = "block";
+    messageDiv.innerHTML = message;
+    messageDiv.style.opacity = 1;
+    setTimeout(function(){
+        messageDiv.style.opacity=0;
+    }, 5000);
+}
+
+//sign up button for signing up
+const signUp = document.getElementById("btnSignup");
+signUp.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    //sets consts
+    const email = document.getElementById("rEmail").value;
+    const pass = document.getElementById("rPass").value;
+    
+    //creates user with said email and pass
+    createUserWithEmailAndPassword(auth, email, pass)
+    .then((userCredential) =>{
+        //sets basic information in database (email)
+        const user = userCredential.user;
+        const userData = {
+            email : email
+        };
+        showMessage("Account created", "signUpMessage");
+        const docRef = doc(db, "users", user.uid);
+        setDoc(docRef, userData)
+        .then(() =>{
+            //on successful creation refs to signin page
+            window.location.href="index.html";
+        })
+        .catch((error) =>{
+            console.error("error writing document", error)
+        })
+    })
+    .catch((error) =>{
+        const errorCode = error.code;
+        if(errorCode == "auth/email-already-in-use"){
+            showMessage("email address exists alr", "signUpMessage");
+        }
+        else{
+            showMessage("unable to create account", "signUpMessage");
+        }
+    })
+})
+
+//sign in button for signing in
+const signIn = document.getElementById("btnLogin");
+signIn.addEventListener("click", (event) =>{
+    event.preventDefault();
+    
+    //settings consts for important information
+    const email = document.getElementById("email").value;
+    const pass = document.getElementById("pass").value;
+    const auth = getAuth(app);
+
+    //signs in
+    signInWithEmailAndPassword(auth, email, pass)
+    .then((userCredential) => {
+        showMessage("login is successful", "loginMessage");
+        const user = userCredential.user;
+        //sets user id in local storage
+        localStorage.setItem("loggedInUserId", user.uid);
+        //refs to main page
+        window.location.href = "main.html";
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        if(errorCode === "auth/invalid-credentials"){
+            showMessage("incorrect email/pass", loginMessage);
+        }
+        else{
+            showMessage("account doesnt exist", loginMessage);
+        }
+    })
+})
+
+
---
+++ main.js
@@ -0,0 +1,205 @@
+//todo: set default values on input boxes to be retrieved from database, add pichart
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore, setDoc, getDoc, doc } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+//chart stuff (loads google charts packages needed for pichart)
+google.charts.load("current", {"packages":["corechart"]});
+
+//basic starter config for firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCVIQW2H64al26iDOgyj_GhVReqwUQ1DMg",
+    authDomain: "financialapp-47aab.firebaseapp.com",
+    databaseURL: "https://financialapp-47aab-default-rtdb.firebaseio.com",
+    projectId: "financialapp-47aab",
+    storageBucket: "financialapp-47aab.appspot.com",
+    messagingSenderId: "1041157429079",
+    appId: "1:1041157429079:web:3b8868693694ca613c1012",
+    measurementId: "G-L86K9E75Q7"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+let date = new Date();
+let currentDay = date.getDate();
+//month : 1-12
+let currentMonth = date.getMonth();
+
+let daysLeft = calcDays(currentDay, currentMonth);
+document.getElementById("d").innerText = "Days left : " + String(daysLeft);
+
+function calcDays(currDay, currMonth){
+    if (currMonth == 1 || currMonth == 3 || currMonth == 5 || currMonth == 7 || currMonth == 8 || currMonth == 10 || currMonth == 12){
+        return 31 - currDay;
+    }
+    if (currMonth == 4 || currMonth == 6 || currMonth == 9 || currMonth == 11){
+        return 30 - currDay;
+    }
+    if(currMonth == 2){
+        year = date.getFullYear();
+        if(year % 400 == 0 || ((year % 4 == 0) && (year % 100 != 0))){
+            return 28 - currDay;
+        }
+        return 29 - currDay;
+    } 
+}
+
+//gets initial values of income, food, bills, misc from database, and sets them to the value in 
+//each appropriate input box
+const currUserId = localStorage.getItem("loggedInUserId");
+const docRef = doc(db, "users", currUserId);
+let startSnap = await getDoc(docRef);
+if(startSnap.exists()){
+    let initI = startSnap.data().totalIncome;
+    let initF = startSnap.data().food;
+    let initB = startSnap.data().bills;
+    let initM = startSnap.data().misc;
+    document.getElementById("i").value = initI;
+    document.getElementById("f").value = initF;
+    document.getElementById("b").value = initB;
+    document.getElementById("m").value = initM;
+}
+else{
+    console.log("init number error")
+}
+//sets numbers for income, food, bills, misc from values in input boxes for later use.
+let i = Number(document.getElementById("i").value);
+let f = Number(document.getElementById("f").value);
+let b = Number(document.getElementById("b").value);
+let m = Number(document.getElementById("m").value);
+
+//gets logged email to put at top left of screen
+onAuthStateChanged(auth, (user) => {
+    if(currUserId){
+        const docRef = doc(db, "users", currUserId);
+        getDoc(docRef)
+        .then((docSnap) => {
+            if(docSnap.exists()){
+                //sets "email: " text at the top right
+                const userData = docSnap.data();
+                document.getElementById("loggedEmail").innerText = userData.email;
+            }
+            else{
+                console.log("no document found matching id");
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+            console.log("error getting document");
+        })
+    }
+    else{
+        console.log("user id not found in local data")
+    }
+})
+
+//button to submit values on budgeting tab
+const budgetEnterButton = document.getElementById("budgetEnterButton");
+budgetEnterButton.addEventListener("click", (event) => {
+    //unpaid bills/dues etc
+    let unpaid = Number(document.getElementById("u").value);
+    var r = 0;
+    if((i - (f + b + m)) >= 0){
+        r = Number(i - (f + b + m));
+    }
+    //setting appropriate values of text boxes/fields
+    document.getElementById("r").innerText = "Remaining Money : " + String(r - unpaid);
+    document.getElementById("budget").innerText = "You can afford to spend : $" + String(Math.floor((r - unpaid)/daysLeft * 100) / 100) + " per day";
+})
+
+//button to submit values on spending breakdown tab
+const enterButton = document.getElementById("enterButton");
+enterButton.addEventListener("click", (event) => {
+    getDoc(docRef)
+    .then((docSnap) => {
+        if(docSnap.exists()){
+            //income, food, bills, misc, updating value based on the values in the appropriate input boxes
+            i = Number(document.getElementById("i").value);
+            f = Number(document.getElementById("f").value);
+            b = Number(document.getElementById("b").value);
+            m = Number(document.getElementById("m").value);
+            setDoc(docRef, {
+                    email: docSnap.data().email,
+                    totalIncome: i,
+                    food: f,
+                    bills: b,
+                    misc: m
+            });
+            drawChart();
+        }
+        else{
+            console.log("no document found matching id");
+        };
+    });
+    
+});
+
+//log out button
+const logoutButton = document.getElementById("logout");
+logoutButton.addEventListener("click", (event) => {
+    //first updates to database
+    getDoc(docRef)
+    .then((docSnap) => {
+        if(docSnap.exists()){
+            //income, food, bills, misc, updating value based on the values in the appropriate input boxes
+            i = Number(document.getElementById("i").value);
+            f = Number(document.getElementById("f").value);
+            b = Number(document.getElementById("b").value);
+            m = Number(document.getElementById("m").value);
+            setDoc(docRef, {
+                    email: docSnap.data().email,
+                    totalIncome: i,
+                    food: f,
+                    bills: b,
+                    misc: m
+            });
+        }
+        else{
+            console.log("no document found matching id");
+        };
+    });
+    //then removes login id from local storage and logs out
+    localStorage.removeItem("loggedInUserId");
+    signOut(auth)
+    .then(() => {
+        window.location.href = "index.html";
+    })
+    .catch((error) => {
+        console.log("Error in signing out", error);
+    });
+});
+
+//draw chart function for google pichart maker
+function drawChart(){
+    //catching negative numbers in the money that is left
+    var r = 0;
+    if((i - (f + b + m)) >= 0){
+        r = Number(i - (f + b + m));
+    }
+    //setting array for google table making library
+    var data = google.visualization.arrayToDataTable([
+        ["Task", "Money Spent"],
+        ["Remaining", Number(r)],
+        ["Food", Number(f)],
+        ["Bills", Number(b)],
+        ["Misc", Number(m)]
+    ]);
+
+    //can add additional options, title was all that is needed
+    var options = {
+        title: "Spending Breakdown",
+        
+    };
+
+    //creates chart object, google visualizer
+    var chart = new google.visualization.PieChart(document.getElementById("piChart"));
+
+    chart.draw(data, options);
+}
+
+
+
+//initial drawChart to load a chart on file load
+drawChart();
