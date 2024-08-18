8eafd37a1f33333662f84a8657b345c1778501ee
LEDLamp1256 <dylanlee0613@gmail.com>
2024-08-17 18:26:35 -0700

    Update firebaseAuth.js

--- firebaseAuth.js
+++ firebaseAuth.js
@@ -22,10 +22,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore();
 
-
-
-
-
 //sign up button for signing up
 const signUp = document.getElementById("btnSignup");
 signUp.addEventListener("click", (event) => {
